b754fecf8b6b3758f08644c5473edb2d5d9e7644
alamin40 <hossain.78295@gmail.com>
2022-01-30 09:46:58 +0600

    first commit

---
+++ app/classes/example.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\classes;
+class example
+{
+    public $name;
+    public $email;
+    public $mobile;
+
+    public function index(){
+     echo 'Hello World';
+    }
+}
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once 'vendor/autoload.php';
+use App\classes\example;
+
+$example = new example();
+$example ->index();
